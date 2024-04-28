bc0ccfad37dfe9c436ce5e235355e22a417ba752
emkartch <elka9063@colorado.edu>
2024-04-28 17:50:15 -0600

    js-final

--- js-final/js-final.js
+++ js-final/js-final.js
@@ -14,7 +14,7 @@
     let rightPressed = false;
     let leftPressed = false;
 
-    const brickRowCount = 3;
+    const brickRowCount = 4;
     const brickColumnCount = 5;
     const brickWidth = 80;
     const brickHeight = 30;
@@ -26,30 +26,19 @@
     var volumeText = document.querySelector('#volumeNum');
     var volumeNum = 0;
 
-
-    var volumeArray = [1,2,3,4,5,6,7,8,9,10,11,12,12]
-
-
-    
-    const randomItem = arr => arr.splice((Math.random() * arr.length) | 0, 1);
-
-
-
-
-
-
-
     let bricks = [];
 
     for (let c = 0; c < brickColumnCount; c++) {
     bricks[c] = [];
     for (let r = 0; r < brickRowCount; r++) {
-        bricks[c][r] = { x: 0, y: 0, status: 1, text: "" };
+        bricks[c][r] = { x: 0, y: 0, status: 1};
     }
     }
 
     document.addEventListener("keydown", keyDownHandler, false);
     document.addEventListener("keyup", keyUpHandler, false);
+
+    document.addEventListener("mousemove", mouseMoveHandler, false);
 
     function keyDownHandler(e) {
     if (e.key == "Right" || e.key == "ArrowRight") {
@@ -67,6 +56,13 @@
     }
     }
 
+    function mouseMoveHandler(e) {
+        const relativeX = e.clientX - gameCanvas.offsetLeft;
+        if (relativeX > 0 && relativeX < gameCanvas.width) {
+          paddleX = relativeX - paddleWidth / 2;
+        }
+      }
+
     function collisionDetection() {
         for (let c = 0; c < brickColumnCount; c++) {
         for (let r = 0; r < brickRowCount; r++) {
@@ -80,8 +76,12 @@
             ) {
                 dy = -dy;
                 b.status = 0;
-                volumeNum += b.text;
+                volumeNum += 4;
                 volumeText.textContent = String(volumeNum);
+                if (volumeNum === 100) {
+                    alert("Volume Set: 100")
+                    document.location.reload();
+                }
 
             }
             }
@@ -92,14 +92,14 @@
     function drawBall() {
         ctx.beginPath();
         ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-        ctx.fillStyle = "#0095DD";
+        ctx.fillStyle = "#646F4B";
         ctx.fill();
         ctx.closePath();
     }
     function drawPaddle() {
         ctx.beginPath();
         ctx.rect(paddleX, gameCanvas.height - paddleHeight, paddleWidth, paddleHeight);
-        ctx.fillStyle = "#0095DD";
+        ctx.fillStyle = "#646F4B";
         ctx.fill();
         ctx.closePath();
     }
@@ -113,17 +113,12 @@
             bricks[c][r].y = brickY;
             ctx.beginPath();
             ctx.rect(brickX, brickY, brickWidth, brickHeight);
-            ctx.fillStyle = "#0095DD";
+            ctx.fillStyle = "#646F4B";
             ctx.fill();
 
-            const brickText = randomItem(volumeArray)
-            bricks[c][r].text = brickText
-
-            console.log(brickText)
-
             ctx.font = "12pt Arial";
-            ctx.fillStyle = "#000000";
-            ctx.fillText(String(brickText), brickX+5, brickY+20);
+            ctx.fillStyle = "#DFE9DF";
+            ctx.fillText("4", brickX+6, brickY+20);
 
             ctx.closePath();
             }
@@ -147,9 +142,8 @@
             if (x > paddleX && x < paddleX + paddleWidth) {
             dy = -dy;
             } else {
-            alert("GAME OVER");
+            alert("Volume Set: " + String(volumeNum));
             document.location.reload();
-            clearInterval(interval); // Needed for Chrome to end game
             }
         }
 
@@ -167,80 +161,16 @@
 
         x += dx;
         y += dy;
+
+        requestAnimationFrame(draw);
     }
 
 // GAME CONTROLS
 
-// VOLUME CODE
-
-    // var volumeCanvas = document.getElementById('volumeCanvas');
-    // var context = volumeCanvas.getContext('2d');
-    // var volume = 0;
-    // var r = 255; 
-    // var g = 0; 
-    // var b = 0;
-    // var volX = 200;
-    // var volY = 50;
-
-    // function volDraw(volume) {
-
-    //     context.clearRect(0, 0, 80, 300);
-
-    //     //bar
-    //     r = 384 - Math.floor(volume / 100 * 384);
-    //     g = Math.floor(volume / 100 * 512);
-        
-    //     if (r < 0) { r = 0; }
-    //     if (r > 255) { r = 255; }
-    //     if (g < 0) { g = 0; }
-    //     if (g > 255) { g = 255; }
-        
-    //     context.fillStyle = "rgb(0,0,0)";
-    //     context.fillRect(volX, volY, 50, 200);
-        
-    //     context.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-    //     context.fillRect(volX, volY - volume * 2 + 200, 50, volume * 2);
-        
-    //     //text
-    //     var percent = Math.floor(volume);
-    //     var textWidth = context.measureText("0").width;
-    //     var digits = Math.floor(Math.log10(percent)) + 1;
-    //     if (digits <= 0) {
-    //         digits = 1;
-    //     }
-    //     var offsetx = textWidth * digits;
-        
-    //     context.fillStyle = "rgb(255,255,255)";
-    //     context.fillText(percent, volX + 40 - offsetx, volY + 75);
-    //     context.font = "24px Arial";
-    //     context.textBaseline = "top";
-    //     context.strokeStyle = "rgb(0,0,0)";
-    //     context.strokeText(percent, volX + 40 - offsetx, volY + 75);
-        
-    //     //emoji
-    //     if (volume > 66) {
-    //         index = 3;
-    //     }
-    //     else if (volume > 33) {
-    //         index = 2;
-    //     }
-    //     else if (volume > 0) {
-    //         index = 1;
-    //     }
-    //     else {
-    //         index = 0;
-    //     }
-        
-    //     context.fillText(speaker[index], volY + 10, volY + 200);
-
-    // }
-
-// VOLUME CODE
-
 // RUN CODE
 
     function setVolume() {
-        let intervalGame = setInterval(draw, 5);
+        draw();
     }
     
     document.getElementById("runButton").addEventListener("click", function () {
